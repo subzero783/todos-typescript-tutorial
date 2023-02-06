d2b7e892e0a3b1511af5d6c34cc48ffda826d8b3
Gustavo Amezcua <gamezcua@ascentfunding.com>
2023-02-06 15:14:26 -0800

    First push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputField/InputField";
+import { Todo } from "./components/Model/model";
 
-function App() {
+// let name: string;
+// let age: number | string;
+
+// age = 31;
+
+// let isStudent: boolean;
+// let hobbies: string[];
+// let role: [number, string];
+
+// let printName:Function;
+
+// "void" returns undefined
+// But "never"
+// let printName: (name: string) => never;
+
+// let personName: unknown;
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// };
+
+// let person: Person = {
+//   name: "Gus",
+// };
+
+// interface Person {
+//   name: string;
+//   age?: number;
+// }
+
+// interface Guy extends Person {
+//   profession: string;
+// }
+
+// Extends the X type with the Person interface
+// type X = Person & {
+//   a: string;
+//   b: number;
+// };
+
+// type X = {
+//   a: string;
+//   b: number;
+// };
+
+// interface Person extends X {
+//   name: string;
+//   age?: number;
+// }
+
+// let lotsOfPeople: Person[];
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          todo: todo,
+          isDone: false,
+        },
+      ]);
+      setTodo("");
+    }
+  };
+
+  console.log(todos);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,26 @@
+import "./InputField.css";
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  return (
+    <form className="input" onSubmit={handleAdd}>
+      <input
+        className=" input__box"
+        type="input"
+        placeholder="Enter a task"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className="input_submit" type="submit">
+        Go
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
